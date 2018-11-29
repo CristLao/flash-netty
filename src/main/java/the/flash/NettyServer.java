76ccabfdd89ffbff6679f72a0476b34d4ad3527f
xiaoxi.lao <xiaoxi.lao@xblgroup.com>
2018-11-29 09:52:01 +0800

    添加注释

--- src/main/java/the/flash/NettyServer.java
+++ src/main/java/the/flash/NettyServer.java
@@ -13,19 +13,33 @@ public class NettyServer {
     private static final int BEGIN_PORT = 8000;
 
     public static void main(String[] args) {
+        // 监听端口
         NioEventLoopGroup boosGroup = new NioEventLoopGroup();
+        // 处理每一条连接的数据读写的线程组
         NioEventLoopGroup workerGroup = new NioEventLoopGroup();
-
+        // 这个类将引导我们进行服务端的启动工作
         final ServerBootstrap serverBootstrap = new ServerBootstrap();
         final AttributeKey<Object> clientKey = AttributeKey.newInstance("clientKey");
         serverBootstrap
+                // 引导类配置两大线程组
                 .group(boosGroup, workerGroup)
+                // 指定我们服务端的 IO 模型为NIO
                 .channel(NioServerSocketChannel.class)
+                // attr()方法可以给服务端的 channel，也就是NioServerSocketChannel指定一些自定义属性，然后我们可以通过channel.attr()
+                // 取出这个属性，比如，下面的代码我们指定我们服务端channel的一个serverName属性，属性值为nettyServer...
                 .attr(AttributeKey.newInstance("serverName"), "nettyServer")
+                // childAttr可以给每一条连接指定自定义属性，然后后续我们可以通过channel.attr()取出该属性。
                 .childAttr(clientKey, "clientValue")
+                // 表示系统用于临时存放已完成三次握手的请求的队列的最大长度
+                // 如果连接建立频繁，服务器处理创建新连接较慢，可以适当调大这个参数
                 .option(ChannelOption.SO_BACKLOG, 1024)
+                // childOption()可以给每条连接设置一些TCP底层相关的属性
+                // ChannelOption.SO_KEEPALIVE表示是否开启TCP底层心跳机制，true为开启
                 .childOption(ChannelOption.SO_KEEPALIVE, true)
+                // ChannelOption.TCP_NODELAY表示是否开启Nagle算法，true表示关闭，false表示开启
+                // 通俗地说，如果要求高实时性，有数据发送时就马上发送，就关闭，如果需要减少发送次数减少网络交互，就开启。
                 .childOption(ChannelOption.TCP_NODELAY, true)
+                // childHandler()用于指定处理新连接数据的读写处理逻辑，handler()用于指定在服务端启动过程中的一些逻辑
                 .childHandler(new ChannelInitializer<NioSocketChannel>() {
                     protected void initChannel(NioSocketChannel ch) {
                         System.out.println(ch.attr(clientKey).get());
